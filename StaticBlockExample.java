b24eecbbb330e7b6e19bbf102e902b7678fbec59
Mahesh Babu <maheshmaheshbabu3@gmail.com>
2018-03-25 15:10:54 +0530

    StaticBlockExample.java
    Static block example

---
+++ StaticBlockExample.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.test;
+
+
+// Print Message Without main method 
+// Note : It will work on before JDK 1.7 version
+public class StaticBlockExample {
+
+	static{
+		System.out.println("Welcome");
+		System.exit(0);
+	}
+
+}
